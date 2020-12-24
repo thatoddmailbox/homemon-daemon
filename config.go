fcbf236c68a147c90e824cca1cf4eb9a97a1ac74
Alex Studer <alex@studer.dev>
2020-12-23 19:24:35 -0500

    config: parse token as bytes, decoding as base64 for udp

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"os"
 
@@ -15,7 +16,8 @@ type config struct {
 	Host string
 
 	// Token sets the token used to authenticate reports.
-	Token string
+	Token      string
+	tokenBytes []byte
 
 	// Transport sets the transport to use to send reports.
 	Transport string
@@ -27,6 +29,22 @@ func loadConfig() error {
 	_, err := toml.DecodeFile("config.toml", &currentConfig)
 	if os.IsNotExist(err) {
 		log.Fatalln("Could not find config file.")
+	} else if err != nil {
+		return err
 	}
-	return err
+
+	currentConfig.tokenBytes = []byte(currentConfig.Token)
+
+	if currentConfig.Transport == "UDP" {
+		currentConfig.tokenBytes, err = base64.URLEncoding.DecodeString(currentConfig.Token)
+		if err != nil {
+			return err
+		}
+
+		if len(currentConfig.tokenBytes) != 64 {
+			log.Fatalf("For UDP transport, token must be exactly 64 bytes long.")
+		}
+	}
+
+	return nil
 }
